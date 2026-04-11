2054d53e3176b4bc08de5729242c92f57dca2191
Rinku Diwakar <prateek.diwakarr@gmail.com>
2026-04-11 12:30:44 +0530

    added a updated resume

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -126,7 +126,7 @@ const HeroSection: React.FC = () => {
                            transform hover:scale-105 bg-blue-100"
               >
                 <a
-                  href="https://drive.google.com/file/d/1aaQO4miLdnErdbSEGEtmBBybjaQZb8qe/view?usp=sharing"
+                  href="https://drive.google.com/file/d/1_hMp3Jj7El9mYVxP1PJ6JN48cbYkFstx/view?usp=sharing"
                   target="_blank"
                   rel="noopener noreferrer"
                   className="flex items-center gap-2 text-gray-900 hover:text-blue-600 transition-all duration-300"
